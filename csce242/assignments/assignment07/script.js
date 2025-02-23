33b1db3f035095107d0869698857398148e6416f
oagho <osasuyiagho@gmail.com>
2025-02-23 14:34:26 -0500

    homework07

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -1,16 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    function addHorizontalLines(numLines) {
-        const container = document.querySelector(".container");
-        const lineSpacing = 80 / (numLines + 1); // Adjust spacing dynamically
+const TREADS = 10;
+const CLIMB_RATE = 32;
 
-        for (let i = 1; i <= numLines; i++) {
-            let horizontalLine = document.createElement("div");
-            horizontalLine.classList.add("horizontal-line");
-            horizontalLine.style.top = `${lineSpacing * i}vh`; // Positioning dynamically
-            container.appendChild(horizontalLine);
-        }
-    }
+const img = document.querySelector("img");
+const climb = document.getElementById("climb");
 
-    // Call the function with the desired number of lines
-    addHorizontalLines(1); // Change the number to add more or fewer lines
-});
+// Creates Stairs
+const DrawStairs = () => {
+  const stairs = document.getElementById("stairs");
+  stairs.classList.add("border-inline");
+  for (let i = 0; i < TREADS; ++i) {
+    const hr = document.createElement("hr");
+    stairs.append(hr);
+  }
+};
+
+const draw = document.getElementById("draw");
+
+draw.onclick = () => {
+  draw.setAttribute("disabled", true);
+  DrawStairs();
+  img.classList.remove("hidden");
+  climb.classList.remove("hidden");
+};
+
+// Makes img "Climb" the stairs
+
+let counter = 0;
+let stepsClimbed = 0;
+let intervalId;
+const array = ["./images/right.png", "./images/left.png"];
+
+const Climb = () => {
+  img.setAttribute("src", array[counter]);
+  const bottom = Number.parseInt(getComputedStyle(img).bottom);
+  img.style.bottom = `${bottom + CLIMB_RATE}px`;
+
+  ++counter;
+  if (counter === 2) counter = 0;
+
+  ++stepsClimbed;
+  if (stepsClimbed === TREADS + 1) clearInterval(intervalId);
+};
+
+climb.onclick = () => {
+  climb.setAttribute("disabled", true);
+  intervalId = setInterval(Climb, 1000);
+};
--- csce242/exercises/o4into-to-js/loop.js
+++ csce242/exercises/o4into-to-js/loop.js
@@ -23,3 +23,54 @@ document.getElementById("btn-display").onclick = () => {
         }
     }
 }
+
+let count = 0;
+let updateCount;
+//let's loop through the numbers and display count
+document.querySelector('#btn-count').onclick = (event) =>{
+    if(event.currentTarget.innerHTML.toLowerCase() == "start"){
+        event.currentTarget.innerHTML = "stop";
+
+        updateCount = setInterval(()=>{
+            count++;
+            document.querySelector("#count-display").innerHTML = count;
+        },300);
+    }
+    //stop the counter
+    else {
+        event.currentTarget.innerHTML = "Start";
+        clearInterval(updateCount);
+    }
+}
+    document.querySelector('#btn-reset-count').onclick = () =>{
+        count = 0;
+        clearInterval(updateCount);
+        document.querySelector("#btn-count").innerHTML = "Start";
+        document.querySelector("#count-display").innerHTML = "";
+    }
+
+    //display  an array of toys
+    document.querySelector('#btn-show-toy').onclick = (event)=> {
+        const toys = ['ball',"skipping rope","car", "doll", "mini car", "elmo"]
+        event.currentTarget.disabled = true;   //can only click this button once
+        // loop throught toys
+        /*
+        for(let i =0; i<toys.length; i++){
+            console.log(toys[i]);
+        }
+        
+        toys.forEach((toy, i)=>{
+            console.log(`${i+1}. ${toy}`);
+        });
+        */
+
+        const ul = document.createElement("ul");
+        document.getElementById("display-toys").append(ul);
+
+        toys.forEach((toy, i)=>{
+            const li = document.createElement("li");
+            ul.append(li);
+            li.innerHTML = `${i+1}. ${toy}`;
+        });
+    }
+
